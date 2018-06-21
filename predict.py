ae921481656874c95aedbfa5d08fdc9a56f1c86e
Dipesh13 <mangwani.dipesh@gmail.com>
2018-06-21 22:14:34 +0530

    added logistic regression for comparison.

--- predict.py
+++ predict.py
@@ -1,6 +1,4 @@
 import pickle
-import os
-
 
 data = """Around Rs 25,000 crore refund is pending for the exporters while more than 3 lakhs applications seeking refunds have piled up with the central government, said West Bengal Finance Minister and former chairman of the empowered committee on GST, Amit Mitra.
 
@@ -13,13 +11,20 @@ The exporters are not getting refund and that is why they are losing out on thei
 with open('oneclass.pickle', 'rb') as fi:
     model = pickle.load(fi)
 
+with open('log.pickle', 'rb') as f:
+    model2 = pickle.load(f)
+
 # foldername = './dataset'
 # filepath = os.path.join(os.getcwd(), foldername)
 # for file in os.listdir(filepath):
 #     with open(os.path.join(filepath, file), 'rb') as f:
 #         data = [f.read()]
 #     label = model.predict(data)
+#     label2 = model2.predict(data)
 #     print (file, label[0])
+#     print (file, label2[0])
 
-label = model.predict([data])
-print (label[0])
+label = model.predict(data)
+label2 = model2.predict(data)
+print (file, label[0])
+print (file, label2[0])
